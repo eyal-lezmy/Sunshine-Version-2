6de9bd57c58fa2f342190bcd3a5a296b148b7bfd
eyal-lezmy <eyal.lezmy+github@gmail.com>
2016-01-27 00:09:41 +0100

    Remove refresh button

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -62,8 +62,6 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Add this line in order for this fragment to handle menu events.
-        setHasOptionsMenu(true);
 
         //we fetch the weather forecast
 //        processForecastRequest();
@@ -86,25 +84,6 @@ public class ForecastFragment extends Fragment {
         }
 
         return forecasts;
-    }
-
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.forecastfragment, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_refresh) {
-            FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
